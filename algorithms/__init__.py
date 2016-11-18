5592d36efd30ea9353ce6ddfb97803e2b7c3424a
marquistj13 <houpeixin@yeah.net>
2016-11-18 15:52:47 +0800

    插图：演示eta更新阈值的必要性:crystal_ball:

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,6 +2,7 @@ import logging
 from npcm import npcm
 from apcm import apcm
 from upcm import upcm
+from npcm_etaWithoutControl import npcm_eta_zero
 
 # log setup
 logger = logging.getLogger('algorithm')
