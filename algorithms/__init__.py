44b9e7ddcc170660b08a3a7d1c1699d0dae9dce3
marquistj13 <houpeixin@yeah.net>
2016-12-04 09:38:11 +0800

    不加入noise elimination而仅依靠调整alpha是无法handle的
    本次explore针对初始化partition中的，background noise
    cluster是否应该显式加入的问题，我发现仅靠设置大的alpha是无法handle的，因此automatic adaptive
    alpha的机制也是行不通的（虽然这个机制我还没有搞出来）

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,6 +3,7 @@ from npcm import npcm
 from apcm import apcm
 from upcm import upcm
 from npcm_etaWithoutControl import npcm_eta_zero
+from npcm_plot_without_background_noise_handle import npcm_without_handle_backnoise
 from npcm_plot import npcm_plot
 
 # log setup
